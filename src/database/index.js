53c3a62eaf2a7c2adbbd3cf77fc100f90c0f13fb
theodikes <gvedichi@gmail.com>
2020-04-28 17:58:03 +0300

    replaced deprecated method 'ensureIndex' with 'createIndex'

--- src/database/index.js
+++ src/database/index.js
@@ -7,7 +7,8 @@ mongoose.Promise = global.Promise;
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
@@ -16,7 +17,7 @@ db.on("connected", () => {
   console.log(`Mongoose connection open on ${uri}`);
 });
 
-db.on("error", err => console.error(err));
+db.on("error", (err) => console.error(err));
 
 db.on("disconnected", () => {
   console.log("mongoose connection disconnected");
